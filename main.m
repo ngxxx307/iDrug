6280fd8df9ae0a4c76ca85a64aaa3402632630c4
ngxxx307 <ngxxx307@umn.edu>
2022-07-21 15:52:51 +0800

    add modelPrediction.csv

--- main.m
+++ main.m
@@ -77,78 +77,80 @@ para = [alpha, beta, gamma];
 
 
 [positiveId, crossval_id] = train_test_split(X{1}, nfolds, scenario);
-disp(size(X{1}))
+
 AUPR = zeros(nfolds,1);
 AUC = zeros(nfolds, 1);
-disp(size(positiveId))
-disp(size(crossval_id))
 
-for fold = 1:nfolds
-    X{1} = yy;
-    PtrainID = positiveId(find(crossval_id~=fold));
-    PtestID  = positiveId(find(crossval_id==fold));
+[U, V, objs] = iDrug(X, w, Au, Av, S, rank1, rank2, para);
+predX = U{1} * V{1}'
+writematrix(predX, 'modelPrediction.csv')
 
-    % sample equal amount of negative sample
-    negativeID = find(X{1}==0);
-    num = numel(negativeID);
-    Nidx = randperm(num);
-    NtestID = negativeID(Nidx(1:length(PtestID)));
+% for fold = 1:nfolds
+%     X{1} = yy;
+%     PtrainID = positiveId(find(crossval_id~=fold));
+%     PtestID  = positiveId(find(crossval_id==fold));
+% 
+%     % sample equal amount of negative sample
+%     negativeID = find(X{1}==0);
+%     num = numel(negativeID);
+%     Nidx = randperm(num);
+%     NtestID = negativeID(Nidx(1:length(PtestID)));
+% 
+%     X{1}(PtestID) = 0; % mask out the test data
+%     
+%     %nid = find(X{1}==0);
+%     %X{1}(nid) = mean(mean(X{1}))+0.05; % initilzation for strategy 2 and 3 to avoild zero block
+% 
+%     tic
+%     [U, V, objs] = iDrug(X, w, Au, Av, S, rank1, rank2, para);
+%     time =toc;
+%     
+%     predX = U{1} * V{1}';
+%     
+%     testScore = [yy(PtestID); yy(NtestID)];
+%     pred = [predX(PtestID); predX(NtestID)];
+%     [auc1, aupr, rocx, rocy, prx, pry] = auc(testScore(:), pred(:), 1e-6);
+% 
+%     if scenario == '2' || scenario == '3'
+%         [B,I] = sort(pred,'descend');
+%         prec = sum(testScore(I(1:k)) == 1) / min(k, sum(testScore == 1));
+%         fprintf('precision %f \n',prec);
+%     end
+% 
+% 
+%     fprintf('%d-Fold: the AUPR score  %d, the AUC score:  %d, running time %f \n', fold, aupr, auc1, time);
+%     AUPR(fold,1) = aupr;
+%     AUC(fold, 1) = auc1;
+% 
+%     
+% 
+% 
+% end
 
-    X{1}(PtestID) = 0; % mask out the test data
-    
-    %nid = find(X{1}==0);
-    %X{1}(nid) = mean(mean(X{1}))+0.05; % initilzation for strategy 2 and 3 to avoild zero block
 
-    tic
-    [U, V, objs] = iDrug(X, w, Au, Av, S, rank1, rank2, para);
-    time =toc;
-    
-    predX = U{1} * V{1}';
-    
-    testScore = [yy(PtestID); yy(NtestID)];
-    pred = [predX(PtestID); predX(NtestID)];
-    [auc1, aupr, rocx, rocy, prx, pry] = auc(testScore(:), pred(:), 1e-6);
-
-    if scenario == '2' || scenario == '3'
-        [B,I] = sort(pred,'descend');
-        prec = sum(testScore(I(1:k)) == 1) / min(k, sum(testScore == 1));
-        fprintf('precision %f \n',prec);
-    end
-
-
-    fprintf('%d-Fold: the AUPR score  %d, the AUC score:  %d, running time %f \n', fold, aupr, auc1, time);
-    AUPR(fold,1) = aupr;
-    AUC(fold, 1) = auc1;
-
-    
-
-
-end
-
-
-auprs = mean(AUPR);
-aucs = mean(AUC);
-fprintf('The averaqge of AUPR and AUC score: %d,  %d \n',  auprs, aucs);
-
-figure(1)
-subplot(1,3,1);
-plot(rocx, rocy);
-xlabel('FPR');
-ylabel('TPR');
-title('(a) AUROC')
-
-subplot(1,3,2);
-plot(prx, pry);
-xlabel('Recall');
-ylabel('Precision');
-title('(a) AUPR')
-
-% check the convergence
-subplot(1,3,3)
-plot(objs);
-xlabel('Number of Iteration');
-ylabel('Objective value');
-title('Convergence')
+% auprs = mean(AUPR);
+% aucs = mean(AUC);
+% fprintf('The averaqge of AUPdispR and AUC score: %d,  %d \n',  auprs, aucs);
+% 
+% figure(1)
+% subplot(1,3,1);
+% plot(rocx, rocy);
+% xlabel('FPR');
+% ylabel('TPR');
+% title('(a) AUROC')
+% 
+% subplot(1,3,2);
+% plot(prx, pry);
+% xlabel('Recall');
+% ylabel('Precision');
+% title('(a) AUPR')
+% 
+% % check the convergence
+% subplot(1,3,3)
+% plot(objs);
+% xlabel('Number of Iteration');
+% ylabel('Objective value');
+% title('Convergence')
 
 end
 
